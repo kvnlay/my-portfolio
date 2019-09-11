4862aa23a74ebbd19f59165277efc42af08d62da
kvnlay <kingstevens007@gmail.com>
2019-09-11 16:13:38 +0100

    Update bio links

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -6,7 +6,7 @@ export default [
   {
     input: 'Kingsley.contactInfo',
     return:
-      '["<a href="mailto:j.chi2241@gmail.com">kkomotayo@gmail.com</a>", "<a rel="noopener" href="https://www.linkedin.com/in/justin-chi-64b12b37">LinkedIn</a>", "<a rel="noopener" href="https://github.com/vai0">github</a>"]',
+      '["<a href="mailto:kkomotayo@gmail.com">kkomotayo@gmail.com</a>", "<a rel="noopener" href="https://www.linkedin.com/in/kingsley-omotayo">LinkedIn</a>", "<a rel="noopener" href="https://github.com/kvnlay">github</a>"]',
   },
   {
     input: 'Kingsley.resume',
